0ed9f01d5f05d65963548f725d563ca39358916e
Tanya Sonker <sonkercodes@gmail.com>
2024-03-01 23:43:22 +0530

    switch theme mode fail

--- main.js
+++ main.js
@@ -1,3 +1,20 @@
 function changeImage(image) {
         image.src = "images/blu.png";
 }
+
+/*
+create 2 files -> light.css, dark.css
+of all the stylesheets, the array picks the first so make sure light.css is first
+with a mpa it's hard b/c navbar isn't a shared component so no state tracking -> duplicated code
+problems changing body's color
+
+function switchMode() {
+        let theme = document.getElementsByTagName('link')[0];
+        if (theme.getAttribute('href') == 'light.css') { 
+                theme.setAttribute('href', 'dark.css'); 
+        } 
+        else { 
+                theme.setAttribute('href', 'light.css'); 
+        } 
+}
+*/
